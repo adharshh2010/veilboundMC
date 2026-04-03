d997daa1d97ed7226f18f0bd96a6528be361021d
adharsh <lordgamingyt648@gmail.com>
2026-04-03 23:04:45 +0530

    INIT OF THE PROJECT

---
+++ src/client/java/com/adharsh/veilbound/client/VeilboundDataGenerator.java
@@ -0,0 +1,11 @@
+package com.adharsh.veilbound.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VeilboundDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
